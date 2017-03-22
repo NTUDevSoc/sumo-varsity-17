2a08f00377ca34180d4ba6df7cbff33997bc2161
deanott <dean.ottewell@diamond.ac.uk>
2017-03-22 18:22:00 +0000

    Added require and boat begin

--- Renderer.py
+++ Renderer.py
@@ -8,7 +8,7 @@ class Renderer:
         pygame.display.set_caption("Pirate Router")
         self.showing = True
         self.backColor = pygame.Color(0, 0, 255)
-    
+
     def render(self):
         while (self.showing):
             for event in pygame.event.get():
@@ -16,8 +16,34 @@ class Renderer:
                     self.showing = False
 
             self.screen.fill(self.backColor)
-            
+
             pygame.display.flip();
 
-renderer = Renderer()
-renderer.render()
+
+"""
+Just dummying the boat
+"""
+class Boat:
+    """
+
+    @param startpo [x,y]
+    @param targetList[[x,y],...n]
+    """
+    def __init__(self, startPos, orderedTargetList):
+        self.pos = startPos
+        self.orderedTargetList = orderedTargetList
+
+
+    """
+    Steps through the target lists
+
+    """
+    def step(self):
+        self.pos = orderedTargetList
+
+
+
+#Added to run when only when called from main
+if __name__ == '__main__':
+    renderer = Renderer()
+    renderer.render()
