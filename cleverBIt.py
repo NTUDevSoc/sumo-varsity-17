6514f4e639afab2a0d6a0a15035e4dfada68db11
Ben Seeley <benjamin.seeley97@gmail.com>
2017-03-22 15:54:41 +0000

    Calculate Route Length Added

--- cleverBIt.py
+++ cleverBIt.py
@@ -1,6 +1,8 @@
 import Point
 
-class Problem
+class Problem:
+    #Start and End are point objects repesenting the ships
+    #island = list [[x,y],..]
     def __init__(self, start, end, island):
         self.start = start
         self.end = end
@@ -21,17 +23,35 @@ class Problem
                 closest = i
         return closest
 
-    def calculateRoute(self, start, end, clockwise):
+    def calculateRouteLength(self, start, end, clockwise):
         currentNode = start
         length = 0
         while (currentNode != end):
             if clockwise:
-                if (currentNode == len(self.island-1)):
+                if (currentNode == len(self.island)-1):
                     nextNode = 0
                 else:
-                    nextNode = currentNode+1 
+                    nextNode = currentNode+1
+            else:
+                if (currentNode == 0):
+                    nextNode = len(self.island)-1
+                else:
+                    nextNode = currentNode-1
+
+            length += self.island[currentNode].distanceTo(self.island[nextNode])
+
+            currentNode = nextNode
+
+        return length
 
     def solve(self):
         islandStart = self.findClosest(self.start)
         islandEnd = self.findClosest(self.end)
+        cwLength = self.calculateRouteLength(islandStart, islandEnd, True)
+        acwLength = self.calculateRouteLength(islandStart, islandEnd, False)
+        if (cwLength < acwLength):
+            return cwLength
+        else:
+            return acwLength
+
 
