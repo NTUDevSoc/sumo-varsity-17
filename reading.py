20d5a9f18f9318444b856d7235d5ed8ae5246f27
Victor <baumbach.victor@gmail.com>
2017-03-22 15:19:54 +0000

    Mucking about

--- reading.py
+++ reading.py
@@ -1,5 +1,7 @@
 
 import io
+import untangle
+import xmltodict
 import xml.etree.ElementTree
 
 
@@ -12,3 +14,13 @@ xmlFile = open("docsAndXML/testcases/sampleTestcase.xml")
 e = xml.etree.ElementTree.parse('docsAndXML/testcases/sampleTestcase.xml').getroot()
 print(e)
 e.get("Point")
+
+obj = untangle.parse('docsAndXML/testcases/sampleTestcase.xml')
+
+print(obj.TestCase.StartPoint[0])
+
+doc = xmltodict.parse(xmlFile.read())
+print(doc)
+print(doc['TestCase'])
+doicskdmsa = {'fun':'done', 'foo':'bar'}
+print(doicskdmsa)
