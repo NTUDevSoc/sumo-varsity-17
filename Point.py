0277c51bf2d34049265f808730dcf3fad5c30197
deanott <dean.ottewell@diamond.ac.uk>
2017-03-22 16:28:01 +0000

    Added notes

--- Point.py
+++ Point.py
@@ -1,10 +1,12 @@
 import math
 
-class Point
+class Point:
+    #Expects number
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
     def distanceTo(self, point):
+        print(self.x.__class__)
         dist = math.sqrt(math.pow((point.x - self.x), 2) + math.pow((point.y - self.y), 2))
         return dist
